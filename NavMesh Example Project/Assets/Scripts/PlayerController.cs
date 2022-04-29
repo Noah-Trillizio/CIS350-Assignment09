b852d1bfaedc3476c3056ccf67f4c6e8f6d67107
DevilbobHellpants <trillizio3@hotmail.com>
2022-04-29 14:46:15 -0500

    Assignment Complete

---
+++ NavMesh Example Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * (Noah Trillizio)
+ * (Assignment 09)
+ * (Controls the movment of the Player)
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter charcater;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+        charcater = GetComponent<ThirdPersonCharacter>();
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            charcater.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            charcater.Move(Vector3.zero, false, false);
+        }
+    }
+}
